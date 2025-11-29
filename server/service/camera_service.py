de713d50e1dc396a5a975d73c2cbaeef18a45eed
minhntn <techit606@gmail.com>
2025-11-29 21:38:00 +0700

    sua cam

--- server/service/camera_service.py
+++ server/service/camera_service.py
@@ -13,7 +13,7 @@ def release_camera(cam: cv2.VideoCapture):
 
 def take_picture() -> str | None:
     print('taking picture...')
-    cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    cam = cv2.VideoCapture(1)
     if cam.isOpened():
         # skip few first frame
         for i in range(10):
